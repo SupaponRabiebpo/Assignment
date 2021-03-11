e677bb14b47519e7d7955ed8f02cdf071af05503
pin <supapon.sp@mail.kmutt.ac.th>
2021-03-12 00:27:33 +0700

    HOmework3

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HWS3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string mode;
+            mode = Console.ReadLine();
+            double x, y ;
+            x = double.Parse(Console.ReadLine());
+            y = double.Parse(Console.ReadLine());
+                if (x >= 0)
+                {
+                    switch (mode)
+                    {
+                        case "time":
+                            Console.WriteLine("{0} {1},{2}", "time", x, Math.Pow(x - 1, 2));
+                            break;
+                        case "price":
+                            Console.WriteLine("{0} {1},{2}", "price", Math.Sqrt(y) + 1, y);
+                            break;
+                        default:
+                            Console.WriteLine("Invalid mode");
+                            break;
+                    }
+
+                }
+                else { Console.WriteLine("Invalid mouse position"); }
+           
+
+
+        }
+	}
+}
